f3ace87eb5f8c1e7e1a35440063a607d33a2e4b4
liaoyizhong <494220571@qq.com>
2016-02-09 21:19:42 +0800

    new files

---
+++ xoyo/js/xoyo.js
@@ -0,0 +1,6 @@
+function show_advert(){
+  document.getElementsByClassName("head_advert_main")[0].style.display="block";
+}
+function hide_advert(){
+  document.getElementsByClassName("head_advert_main")[0].style.display="none";
+}
